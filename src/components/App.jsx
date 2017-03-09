426cddd131cd2bba75dbfdf37a33aa8324d2b660
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-03-09 18:13:13 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+
+
+class App extends React.Component{
+    render() {
+        return (
+            <div id="app-component">
+                <h1 className="header"> App Component </h1>
+            </div>
+        )
+    }
+}
+
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'index.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        HtmlWebpackPluginConfig
+    ]
+}
