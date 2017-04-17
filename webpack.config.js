65206ff7caf44e93538456a165ed0132b2863be1
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-04-16 22:26:09 -0400

    updates webpack config to auto bundle icons

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractCSS = require('extract-text-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
 
 // storing vendor packages and libraries
 const VENDOR_LIBS = [
@@ -42,8 +44,16 @@ const config = {
                 exclude: /node_modules/
             },
             {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                loader: 'url-loader?limit=100000',
+                test: /\.(jpe?g|png|gif|svg|ico)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 40000
+                        }
+                    },
+                    'image-webpack-loader'
+                ],
                 exclude: /node_modules/
             }
         ]
@@ -53,6 +63,34 @@ const config = {
             template: './src/index.html',
             filename: 'index.html',
             inject: 'body'
+        }),
+        new FaviconsWebpackPlugin({
+            // Your source logo
+            logo: './src/assets/images/favicon/favicon.svg',
+            // The prefix for all image files (might be a folder or a name)
+            prefix: 'icon-[name]-[hash]/',
+            // Emit all stats of the generated icons
+            emitStats: false,
+            // Generate a cache file with control hashes and
+            // don't rebuild the favicons until those hashes change
+            persistentCache: true,
+            // Inject the html into the html-webpack-plugin
+            inject: true,
+            // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+            background: '#fff',
+            // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+            icons: {
+              android: false,
+              appleIcon: false,
+              appleStartup: false,
+              coast: false,
+              favicons: true,
+              firefox: true,
+              opengraph: false,
+              twitter: false,
+              yandex: false,
+              windows: false
+            }
         }),
         new webpack.optimize.CommonsChunkPlugin({
             names: ['vendor', 'manifest'],
